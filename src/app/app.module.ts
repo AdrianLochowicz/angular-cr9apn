187da45a9b70428db31d5fb1d23d0de8092a75bd
AdrianLochowicz <la36856@zut.edu.pl>
2019-12-11 21:59:38 +0100

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WydatkiService } from './wydatki.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [WydatkiService]
 })
 export class AppModule { }
