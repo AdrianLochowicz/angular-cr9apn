52169cc00b710fccdb557e34f1f239014d5b37a2
AdrianLochowicz <la36856@zut.edu.pl>
2019-12-14 11:59:57 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,9 @@ ngOnInit() {
         new Wydatek('Myjnia', 75, '', new Date(2018, 4, 2)),
         new Wydatek('Serwis', 1250, 'Klocki, rozrząd i parę drobiazgów', new Date(2018, 3, 16)),
         new Wydatek('Inne', 315.50, 'Różowe futerko na kierownicę', new Date(2018, 3, 16)),
-        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12))
+        new Wydatek('Tankowanie', 325.20, 'Do pełna po urlopie', new Date(2017, 7, 12)), 
+        new Wydatek('Tankowanie',285, 'pierwszy wpis', new Date(2017, 7, 12), 18, 5),
+        new Wydatek('Tankowanie',285, 'pierwszy wpis', new Date(2017, 7, 12), 9, 13)
     ];
 }
 }
