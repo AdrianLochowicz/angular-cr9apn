0cf638854a7024fbb13369305d08184d2d18c7c9
aksyzn <la36856@zut.edu.pl>
2020-01-14 20:01:39 +0100

    test

--- src/app/lista-wydatkow/lista-wydatkow.component.ts
+++ src/app/lista-wydatkow/lista-wydatkow.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { WydatkiService } from '../wydatki.service';
+import { Wydatek, KATEGORIE } from '../app/Wydatek';
+import { WydatkiService } from '../app/wydatki.service';
 
 @Component({
   selector: 'app-lista-wydatkow',
   templateUrl: './lista-wydatkow.component.html',
   styleUrls: ['./lista-wydatkow.component.css']
 })
-export class ListaWydatkowComponent implements OnInit 
-{
-constructor(private wydatkiService: WydatkiService) { }
+export class ListaWydatkowComponent implements OnInit {
+
+ wydatki: Wydatek[];
+
+ constructor(private wydatkiService: WydatkiService) {}
+
+   ngOnInit() {
+     this.wydatki = this.wydatkiService.getWydatki();
+   }
+
 }
