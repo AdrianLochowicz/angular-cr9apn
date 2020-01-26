8d76438610afe60596d4a5d2dded07e1205d57e5
AdrianLochowicz <la36856@zut.edu.pl>
2020-01-26 18:57:47 +0100

    2 files modified

--- src/app/sumawydatkow/sumawydatkow.component.ts
+++ src/app/sumawydatkow/sumawydatkow.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Wydatek, KATEGORIE } from './app/Wydatek';
+import { Wydatek, KATEGORIE } from '../wydatek';
 import { WydatkiService } from '../wydatki.service';
 
 @Component({
@@ -12,10 +12,11 @@ export class SumawydatkowComponent implements OnInit {
  wydatki: Wydatek[];
 
  constructor(private wydatkiService: WydatkiService) {}
-
+public wydatki2;
   ngOnInit() 
   {
        this.wydatki = this.wydatkiService.getWydatki();
+          this.wydatki2 = this.getWydatkizBierzacegomiesiaca();
   }
 getSuma(): number
 {
@@ -27,7 +28,7 @@ return suma;
 getSumazKategori(category): number
 {
 let suma = 0;
-for (let i = 0; i < this.wydatki.length; i++)
+for (let i = 0; i < this.wydatki.length; i++) 
 {
 if(this.wydatki[i].kategoria == category)
 {suma += this.wydatki[i].kwota;}
@@ -36,11 +37,27 @@ return suma;
 }
 getWydatkizBierzacegomiesiaca()
 {
-  let suma = 0;
-  for (let i = 0; i < this.wydatki.length; i++)
-  {
-    if(this.wydatki[i].data == '19.01.2020')
-    {return this.wydatki[i].kwota;}
+    var wydatki_per_month = [];
+    var date1 = new Date();
+    var n = date1.getMonth();
+    var y = date1.getYear();
+    for (let i = 0; i < this.wydatki.length; i++)
+    {
+      if(this.wydatki[i].data.getMonth() == n && this.wydatki[i].data.getYear() == y)
+      {
+        wydatki_per_month.push(this.wydatki[i]);
+      }
+    }
+    return wydatki_per_month;
   }
-}
+  showBiezacyMiesiac()
+  {
+    document.getElementById('CurrentMonth').style.display = 'block';
+  document.getElementById('Full').style.display = 'none';
+  }
+  showAll()
+  {
+    document.getElementById('CurrentMonth').style.display = 'none';
+  document.getElementById('Full').style.display = 'block';
+  }
 }
