82dfb5b1fee4798db10e2af37c2417b1d6d56cdb
AdrianLochowicz <la36856@zut.edu.pl>
2020-01-28 19:00:03 +0100

    2 files modified

--- src/app/sumawydatkow/sumawydatkow.component.ts
+++ src/app/sumawydatkow/sumawydatkow.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Wydatek, KATEGORIE } from "../wydatek";
 import { WydatkiService } from "../wydatki.service";
+import { formatDate } from "@angular/common";
 
 @Component({
   selector: "app-sumawydatkow",
@@ -10,10 +11,11 @@ import { WydatkiService } from "../wydatki.service";
 })
 export class SumawydatkowComponent implements OnInit {
   wydatki: Wydatek[];
-
+  public sw = 1;
   constructor(private wydatkiService: WydatkiService) {}
   public wydatki2;
   ngOnInit() {
+    this.sw = 1;
     this.wydatki = this.wydatkiService.getWydatki();
     this.wydatki2 = this.getWydatkiByCurrentMonth();
   }
@@ -33,28 +35,69 @@ export class SumawydatkowComponent implements OnInit {
     }
     return suma;
   }
-  getWydatkiByCurrentMonth() {
-    var wydatki_per_month = [];
+
+  getSumazKategoriMiesiac2(): number {
     var date1 = new Date();
     var n = date1.getMonth();
     var y = date1.getYear();
-    console.log(n);
+    let suma = 0;
+
     for (let i = 0; i < this.wydatki.length; i++) {
-      if (
-        this.wydatki[i].data.getMonth() == n &&
-        this.wydatki[i].data.getYear() == y
-      ) {
+      const myDate = this.wydatki[i].data;
+      var wydData = new Date(myDate);
+
+      if (wydData.getMonth() == n && wydData.getYear() == y) {
+        suma += this.wydatki[i].kwota;
+      }
+    }
+
+    return suma;
+  }
+
+  getSumazKategoriMiesiac(category): number {
+    var date1 = new Date();
+    var n = date1.getMonth();
+    var y = date1.getYear();
+    let suma = 0;
+
+    for (let i = 0; i < this.wydatki.length; i++) {
+      const myDate = this.wydatki[i].data;
+      var wydData = new Date(myDate);
+
+      if (wydData.getMonth() == n && wydData.getYear() == y) {
+        if (this.wydatki[i].kategoria == category) {
+          suma += this.wydatki[i].kwota;
+        }
+      }
+    }
+
+    return suma;
+  }
+
+  getWydatkiByCurrentMonth() {
+    var wydatki_per_month = [];
+    var date1 = new Date();
+
+    var n = date1.getMonth();
+    var y = date1.getYear();
+
+    for (let i = 0; i < this.wydatki.length; i++) {
+      const myDate = this.wydatki[i].data;
+      var wydData = new Date(myDate);
+      if (wydData.getMonth() == n && wydData.getYear() == y) {
         wydatki_per_month.push(this.wydatki[i]);
       }
     }
     return wydatki_per_month;
   }
   showCurrentMonth() {
+    this.sw = 0;
     document.getElementById("CurrentMonth").style.display = "block";
     document.getElementById("Full").style.display = "none";
   }
   showFull() {
     document.getElementById("CurrentMonth").style.display = "none";
     document.getElementById("Full").style.display = "block";
+    this.sw = 1;
   }
 }
