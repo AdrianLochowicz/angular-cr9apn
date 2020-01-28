d0861cab361c5f27eda23188efd6f989952f90d7
AdrianLochowicz <la36856@zut.edu.pl>
2020-01-28 17:18:52 +0100

    2 files modified

--- src/app/dodaj-wydatek/dodaj-wydatek.component.ts
+++ src/app/dodaj-wydatek/dodaj-wydatek.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { WydatkiService } from '../wydatki.service';
-import { Wydatek } from '../wydatek';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { WydatkiService } from "../wydatki.service";
+import { Wydatek } from "../wydatek";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-dodaj-wydatek',
-  templateUrl: './dodaj-wydatek.component.html',
-  styleUrls: ['./dodaj-wydatek.component.css']
+  selector: "app-dodaj-wydatek",
+  templateUrl: "./dodaj-wydatek.component.html",
+  styleUrls: ["./dodaj-wydatek.component.css"]
 })
 export class DodajWydatekComponent implements OnInit {
- nowyWydatek: Wydatek;
-  
-kategorie: string[];
+  nowyWydatek: Wydatek;
 
-constructor(private wydatkiService: WydatkiService, private router: Router) { }
+  kategorie: string[];
+
+  constructor(private wydatkiService: WydatkiService, private router: Router) {}
 
   ngOnInit() {
-this.nowyWydatek = new Wydatek(null, 'Tankowanie', null, null, null);
-  this.kategorie = this.wydatkiService.getKategorie();
+    this.nowyWydatek = new Wydatek(null, "Tankowanie", null, null, null);
+    this.kategorie = this.wydatkiService.getKategorie();
   }
   onSubmit() {
     this.wydatkiService.dodajWydatek(this.nowyWydatek);
-      this.nowyWydatek = new Wydatek(null, 'Tankowanie', null, null, null);
-      this.router.navigate(['/wydatki']);
+    this.nowyWydatek = new Wydatek(null, "Tankowanie", null, null, null);
+    this.router.navigate(["/wydatki"]);
   }
 }
